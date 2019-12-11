cb2761d9bd28092e2b3c0041f9a8c4bcb32f5485
F1L1P3P4UL1N0 <fpb@discente.ifpe.edu.br>
2019-12-11 11:39:03 -0300

    Atualizações da aplicação

--- src/app/model/product.model.ts
+++ src/app/model/product.model.ts
@@ -1,10 +1,11 @@
-export class Product {
 
+export class Product {
+ 
     constructor(
         public id?: number,
         public name?: string,
         public category?: string,
         public description?: string,
-        public price?: number
-    ){ }
+        public price?: number,
+        public linkI?: any){ }
 }
